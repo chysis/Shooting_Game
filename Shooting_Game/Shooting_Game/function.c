7a310ac91d879a13eba6f41a9f601b4523007d6b
chysis <chysiss@naver.com>
2020-11-12 00:02:27 +0900

    전체적인 변수/함수 이름 수정, How To Play 기능 삭제

--- Shooting_Game/Shooting_Game/function.c
+++ Shooting_Game/Shooting_Game/function.c
@@ -9,32 +9,50 @@
 #include <stdlib.h>
 #define stdhdl GetStdHandle(STD_OUTPUT_HANDLE)
 
-#define title_cursor 40
+#define title_cursor 40                // 메인 화면 커서의 x좌표 값
 
-int area = 0;   // 모드 스위치 (0: title, 1: Game Field, 2: Ranking, 3: How to Play) #TODO
+// 콘솔 관리 함수
+
+void Gotoxy(short x, short y);         // 커서 이동 함수
+void SetConsoleSize();                 // 콘솔창 크기 조절 함수
+void HideCursor(bool visibility);      // 커서 숨기는 함수
+void SetColor(int txt, int bg);        // 커서 색상, 배경 색상 지정 함수
+
+// 메인 메뉴 관련 함수
+
+void DrawTitle();                      // 타이틀 화면 출력 함수
+void PrintMenu();                      // 메인 메뉴 출력 함수
+void DeleteCursor(int d);              // 메인 화면 '>>' 커서 지우는 함수
+void ArrowCursor(int a);               // 메인 화면 '>>' 커서 출력 함수
+int ShowCur();                         // 메인 화면의 '>>' 커서 위치 불러오기
+void UpdownCur(int num);               // 메인 화면 커서 이동 함수
+void GetArrowKey(); 
+
+// 전역 변수
+
+int area = 0;   // 모드 스위치 (0: title, 1: How to Play, 2: Game Field, 3: Ranking) #TODO
 int title_default_cur = 25;   // 타이틀 화면 커서 초기 위치.
-
-							  // 콘솔 색상 모음 (0~15)
-enum colorset {
+						  
+enum eColorSet { // 콘솔 색상 모음 (0~15)
 	Black = 0, Blue, Green, Cyan, Red, Magenta, Brown, Lightgray, Darkgray,
 	Lightblue, Lightgreen, Lightcyan, Lightred, Lightmagenta, Yellow, White = 15
 };
 
-void gotoxy(short x, short y)
+void Gotoxy(short x, short y)
 // 커서를 (x, y)좌표로 이동. 
 {
 	COORD pos = { x, y }; // x, y좌표
 	SetConsoleCursorPosition(stdhdl, pos); // set cursor
 }
 
-void SetConsolesize()
+void SetConsoleSize()
 // 콘솔 창 크기를 조정
 {
-	system("mode con:lines=50 cols=100"); // lines=세로, cols=가로
+	system("mode con:lines=40 cols=100"); // lines=세로, cols=가로
 }
 
 void HideCursor(bool visibility)
-// 커서를 숨긴다.(visibility true: 보임, false: 안 보임)
+// 커서를 숨긴다. (visibility true: 보임, false: 안 보임)
 {
 	CONSOLE_CURSOR_INFO ConsoleCursor = { 0, };
 	ConsoleCursor.dwSize = 1;
@@ -42,8 +60,8 @@ void HideCursor(bool visibility)
 	SetConsoleCursorInfo(stdhdl, &ConsoleCursor);
 }
 
-void Setcolor(int txt, int bg)
-// txt: 텍스트 색상, bg: 배경 색상. 색상표는 열거형 colorset 참고.
+void SetColor(int txt, int bg)
+// txt: 텍스트 색상, bg: 배경 색상. 색상표는 열거형 eColorSet 참고.
 {
 	SetConsoleTextAttribute(stdhdl, txt + bg * 16);
 }
@@ -53,15 +71,15 @@ void DrawTitle()
 {
 	int x = 15;
 	int y = 10;
-	gotoxy(x, y);
+	Gotoxy(x, y);
 
-	printf("          ########         #         ##         ##   ##########"); gotoxy(x, y + 1);
-	printf("         ##               # #        # #       # #   #         "); gotoxy(x, y + 2);
-	printf("        ##               #   #       #  #     #  #   #         "); gotoxy(x, y + 3);
-	printf("       ##    #####      #######      #   #   #   #   ##########"); gotoxy(x, y + 4);
-	printf("        ##      ##     #       #     #   #   #   #   #         "); gotoxy(x, y + 5);
-	printf("         ##     ##    #         #    #    # #    #   #         "); gotoxy(x, y + 6);
-	printf("          ########   #           #   #     #     #   ##########"); gotoxy(x, y + 7);
+	printf("          ########         #         ##         ##   ##########"); Gotoxy(x, y + 1);
+	printf("         ##               # #        # #       # #   #         "); Gotoxy(x, y + 2);
+	printf("        ##               #   #       #  #     #  #   #         "); Gotoxy(x, y + 3);
+	printf("       ##    #####      #######      #   #   #   #   ##########"); Gotoxy(x, y + 4);
+	printf("        ##      ##     #       #     #   #   #   #   #         "); Gotoxy(x, y + 5);
+	printf("         ##     ##    #         #    #    # #    #   #         "); Gotoxy(x, y + 6);
+	printf("          ########   #           #   #     #     #   ##########"); Gotoxy(x, y + 7);
 }
 
 void PrintMenu()
@@ -69,43 +87,41 @@ void PrintMenu()
 {
 	int x = 45;
 	int y = 25;
-	gotoxy(x, y);
-	Setcolor(Lightred, Black);
+	Gotoxy(x, y);
+	SetColor(Lightred, Black);
 	printf("Game Start");
 
-	gotoxy(x, y + 5);
+	Gotoxy(x, y + 5);
 	printf("Ranking");
 
-	gotoxy(x, y + 10);
-	printf("How to Play?");
-
-	gotoxy(x, y + 15);
+	Gotoxy(x, y + 10);
 	printf("Exit");
-	Setcolor(White, Black);
-	gotoxy(0, 0);
+
+	SetColor(White, Black);
+	Gotoxy(0, 0);
 }
 
 void DeleteCursor(int d)
 // 커서 제거
 {
-	gotoxy(title_cursor, d);
+	Gotoxy(title_cursor, d);
 	printf("  ");
 }
 
 void ArrowCursor(int a)
 // 메뉴 선택용 화살표 커서
 {
-	gotoxy(title_cursor, a);  // 초기 위치: Game Start
+	Gotoxy(title_cursor, a);  // 초기 위치: Game Start
 	printf(">>");
 }
 
-int Showcur()
-// 전역변수 cur값 불러오기
+int ShowCur()
+// 메인 화면의 >> 커서 값 불러오기
 {
 	return title_default_cur;
 }
 
-void Updowncur(int num)
+void UpdownCur(int num)
 // 전역변수 cur값 조정
 {
 	title_default_cur += num;
@@ -116,7 +132,7 @@ void GetArrowKey()
 {
 	while (area == 0)  // 초기 화면에서만 동작 
 	{
-		int a = Showcur();  // 변경 이전의 커서값 저장
+		int a = ShowCur();  // 변경 이전의 커서값 저장
 		int temp;
 		temp = _getch();
 		if (temp == 224)  // 방향키를 입력받았을 때
@@ -124,21 +140,21 @@ void GetArrowKey()
 			temp = _getch();
 			if (temp == 72)  // 위 방향키
 			{
-				if (Showcur() != 25)
+				if (ShowCur() != 25)
 				{
-					Updowncur(-5);
+					UpdownCur(-5);
 					DeleteCursor(a);
-					ArrowCursor(Showcur());
+					ArrowCursor(ShowCur());
 					continue;
 				}
 			}
 			else if (temp == 80)  // 아래 방향키
 			{
-				if (Showcur() != 40)
+				if (ShowCur() != 35)
 				{
-					Updowncur(5);
+					UpdownCur(5);
 					DeleteCursor(a);
-					ArrowCursor(Showcur());
+					ArrowCursor(ShowCur());
 					continue;
 				}
 			}
@@ -154,11 +170,7 @@ void GetArrowKey()
 			{
 				// #TODO Ranking 기능
 			}
-			else if (a == 35)  // How to Play?
-			{
-				// #TODO How to Play 기능
-			}
-			else if (a == 40)  // Exit
+			else if (a == 35)  // Exit
 			{
 				exit(1);
 			}
@@ -168,12 +180,12 @@ void GetArrowKey()
 
 int main(void)
 {
-	HideCursor(0);  // 커서 숨기기
-	SetConsolesize();  // 화면 크기 설정
-	DrawTitle();
-	PrintMenu();
-	ArrowCursor(25);
-	GetArrowKey();
+	HideCursor(0);     // 커서 숨기기
+	SetConsoleSize();  // 화면 크기 설정
+	DrawTitle();       // 타이틀 출력
+	PrintMenu();       // 메인 메뉴 출력
+	ArrowCursor(25);   // 메인 메뉴 커서 출력
+	GetArrowKey();     // 커서 이동 함수
 
 	return 0;
 }
