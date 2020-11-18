ccae55edd216db7d1a4bc651145c9db8f132936c
chysis <chysiss@naver.com>
2020-11-18 19:01:10 +0900

    비행기 선택화면 구현, 수정

--- Shooting_Game/Shooting_Game/function.c
+++ Shooting_Game/Shooting_Game/function.c
@@ -7,11 +7,12 @@
 #include <time.h>
 #include <stdbool.h>
 #include <stdlib.h>
+
 #define stdhdl GetStdHandle(STD_OUTPUT_HANDLE)
-
 #define title_cursor 40                // 메인 화면 커서의 x좌표 값
-
-/* 이곳에서 모든 함수를 확인하세요 */
+#define plane_cursor 14                // 비행기 선택화면 커서의 y좌표 값                
+#define MAX_ENEMY 10                   // 적군 최대 수
+#define MAX_BALL 20                    // 적군 최대 총알 수
 
 // 콘솔 관리 함수
 
@@ -19,38 +20,109 @@ void Gotoxy(short x, short y);         // 커서 이동 함수
 void SetConsoleSize();                 // 콘솔창 크기 조절 함수
 void HideCursor(bool visibility);      // 커서 숨기는 함수
 void SetColor(int txt, int bg);        // 커서 색상, 배경 색상 지정 함수
+BOOL IsKeyDown(int Key);               // 키보드가 눌려있는지 판단
 
-// 메인 메뉴 관련 함수
+									   // 메인 메뉴 관련 함수
 
 void DrawTitle();                      // 타이틀 화면 출력 함수
 void PrintMenu();                      // 메인 메뉴 출력 함수
-void DeleteCursor(int d);              // 메인 화면 '>>' 커서 지우는 함수
-void ArrowCursor(int a);               // 메인 화면 '>>' 커서 출력 함수
-void GetArrowKey();                    // 메인 화면 커서 입력받는 함수
+void DeleteCursor(int d, int type);    // 메인 화면 '>>' 커서 지우는 함수
+void ArrowCursor(int a, int type);     // 메인 화면 '>>' 커서 출력 함수
+void GetTitleArrowKey();               // 메인 화면 커서 입력받는 함수
 
-// 게임 구현 함수
+									   // 게임 구현 함수
 
 void HowToPlay();                      // 플레이 방법 출력 함수
-void SelectPlaneUI();                  // 플레이어 유닛 선택 UI 출력 함수
-void SelectPlane();                    // 플레이어 유닛 선택 함수
+void SelectPlane();                    // 플레이어 유닛 선택 함수 (UI 포함)
+void GetPlaneArrowKey();               // 유닛 선택 화면 커서 함수
+void ShootingGameUI();                 // 게임 UI출력 함수
+void ShootingGame();                   // 게임 함수
+void GetPlayerName();                  // 게임 종료 후 이름 입력 받는 함수
 
-// 랭킹 구현 함수
+									   // 랭킹 구현 함수
 
 void RankingUI();                      // 랭킹 화면 UI 출력 함수
 
-// 전역 변수, 열거형, 구조체
+									   // 전역 변수, 열거형, 구조체
 
-int area = 0;   // 모드 스위치 (0: title, 1: How to Play, 2: Game Field, 3: Ranking) #TODO
+int planeCode;      // 플레이어가 선택한 비행기 코드
+int area = 0;   // 모드 스위치 (0: title, 1: How to Play, 2: Select Plane, 3: Game Field, 4: Ranking) #TODO
+int score;      // 게임 점수
 int title_default_cur = 25;   // 타이틀 화면 커서 초기 위치.
 
-int titleCursor[3] = { 25, 30, 35 };   // 타이틀 커서의 y좌표값 배열
-						  
-enum eColorSet { // 콘솔 색상 모음 (0~15)
+int titleCursor[3] = { 25, 30, 35 };      // 타이틀 커서의 y좌표값 배열
+int planeCursor[4] = { 19, 39, 59, 79 };  // 비행기 선택 커서의 x좌표값 배열
+
+enum ColorSet { // 콘솔 색상 모음 (0~15)
 	Black = 0, Blue, Green, Cyan, Red, Magenta, Brown, Lightgray, Darkgray,
 	Lightblue, Lightgreen, Lightcyan, Lightred, Lightmagenta, Yellow, White = 15
 };
 
+typedef struct bullet { // 총알의 위치를 알려주는 구조체
+	int x, y;
+} bullet;
+
+typedef struct user_stat { // 플레이어의 스텟
+	int x, y;
+	int life;
+	int check;
+	bullet bullet;
+} User;
+
+struct enemy_stat {  // 적군의 스텟
+	BOOL exist;
+	int Type;
+	int x, y;
+	int Delta;
+	int nFrame;
+	int nStay;
+} Enemy[MAX_ENEMY];
+
+struct ball_stat {  // 총알의 스텟
+	BOOL exist;
+	int x, y;
+	int nFrame;
+	int nStay;
+} Ball[MAX_BALL];
+
+char *arEnemy[] = { "oOoOo", "[-@-]", "^_*_^" };  // 적 유닛 배열
+
+int plane[4][3][3] = { // 플레이어 유닛 배열
+	{
+		{ 1,0,1 },
+		{ 0,1,0 },
+		{ 1,0,1 }
+	},
+	{
+		{ 1,0,1 },
+		{ 1,1,1 },
+		{ 1,0,1 }
+	},
+	{
+		{ 1,1,1 },
+		{ 0,1,0 },
+		{ 0,1,0 }
+	},
+	{
+		{ 0,1,0 },
+		{ 1,0,1 },
+		{ 0,1,0 }
+	}
+};
+
+/*
+const int plane1[3][3] = { { 1,0,1 },{ 0,1,0 },{ 1,0,1 } }; //X
+const int plane2[3][3] = { { 1,0,1 },{ 1,1,1 },{ 1,0,1 } }; //H
+const int plane3[3][3] = { { 1,1,1 },{ 0,1,0 },{ 0,1,0 } }; //T
+const int plane4[3][3] = { { 0,1,0 },{ 1,0,1 },{ 0,1,0 } }; //O
+*/
+
 /* 구현 */
+
+BOOL IsKeyDown(int Key)
+{
+	return ((GetAsyncKeyState(Key) & 0x8000) != 0);
+}
 
 void Gotoxy(short x, short y)
 // 커서를 (x, y)좌표로 이동. 
@@ -75,7 +147,7 @@ void HideCursor(bool visibility)
 }
 
 void SetColor(int txt, int bg)
-// txt: 텍스트 색상, bg: 배경 색상. 색상표는 열거형 eColorSet 참고.
+// txt: 텍스트 색상, bg: 배경 색상. 색상표는 열거형 ColorSet 참고.
 {
 	SetConsoleTextAttribute(stdhdl, txt + bg * 16);
 }
@@ -115,25 +187,45 @@ void PrintMenu()
 	Gotoxy(0, 0);
 }
 
-void DeleteCursor(int d)
-// 커서 제거
+void DeleteCursor(int d, int type)
+// 커서 제거 (type 1: 메인화면, 2: 비행기 선택화면)
 {
-	Gotoxy(title_cursor, d);
-	printf("  ");
+	if (type == 1)
+	{
+		Gotoxy(title_cursor, d);
+		printf("  ");
+	}
+	else if (type == 2)
+	{
+		Gotoxy(d, plane_cursor);
+		printf("  ");
+	}
+	else
+		return;
 }
 
-void ArrowCursor(int a)
-// 메뉴 선택용 화살표 커서
+void ArrowCursor(int a, int type)
+// 메뉴 선택용 화살표 커서 (type 1: 메인화면, 2: 비행기 선택화면)
 {
-	Gotoxy(title_cursor, a);  // 초기 위치: Game Start
-	printf(">>");
+	if (type == 1)
+	{
+		Gotoxy(title_cursor, a);  // 초기 위치: Game Start
+		printf(">>");
+	}
+	else if (type == 2)
+	{
+		Gotoxy(a, plane_cursor);
+		printf("△");
+	}
+	else
+		return;
 }
 
-void GetArrowKey()
+void GetTitleArrowKey()
 // 타이틀 화면에서 방향키를 입력받아 커서를 움직인다.
 {
 	int idx = 0;                  // 범위: 0~2
-	ArrowCursor(titleCursor[0]);  // 초기 커서 위치
+	ArrowCursor(titleCursor[0], 1);  // 초기 커서 위치
 	while (area == 0)             // 초기 화면에서만 동작 
 	{
 		int temp;
@@ -143,35 +235,36 @@ void GetArrowKey()
 			temp = _getch();
 			if (temp == 72)       // 위 방향키
 			{
-				if (idx!=0)
+				if (idx != 0)
 				{
-					DeleteCursor(titleCursor[idx--]);
-					ArrowCursor(titleCursor[idx]);
+					DeleteCursor(titleCursor[idx--], 1);
+					ArrowCursor(titleCursor[idx], 1);
 					continue;
 				}
 			}
 			else if (temp == 80)   // 아래 방향키
 			{
-				if (idx!=2)
+				if (idx != 2)
 				{
-					DeleteCursor(titleCursor[idx++]);
-					ArrowCursor(titleCursor[idx]);
+					DeleteCursor(titleCursor[idx++], 1);
+					ArrowCursor(titleCursor[idx], 1);
 					continue;
 				}
 			}
 		}
 		else if (temp == 13)    // 엔터키를 입력받았을 때
 		{
-			if (idx==0)         // #TODO 게임 시작
+			if (idx == 0)         // #TODO 게임 시작
 			{
-				area = 1;       // 초기화면에서 벗어남
+				area = 1;
 				HowToPlay();
 			}
-			else if (idx==1)   // Ranking
+			else if (idx == 1)   // Ranking
 			{
+				area = 2;
 				// #TODO Ranking 기능
 			}
-			else if (idx==2)   // Exit
+			else if (idx == 2)   // Exit
 			{
 				exit(1);
 			}
@@ -185,7 +278,7 @@ void HowToPlay()
 	int temp;
 
 	system("cls");
-	Gotoxy(x, y); 
+	Gotoxy(x, y);
 	printf("***********************< How To Play? >*********************\n");
 	for (int i = 0; i < 18; i++)
 	{
@@ -205,53 +298,117 @@ void HowToPlay()
 	Gotoxy(30, 26); printf("엔터키를 누르시면 다음 화면으로 넘어갑니다...");
 	SetColor(0, 0);
 
-	while (1)
+	if (area == 1)
 	{
-		temp = _getch();
-		if (temp == 13)        // 엔터키 입력받았을 때
-			SelectPlaneUI();   // 비행기 선택 화면으로 이동
+		while (1)
+		{
+			temp = _getch();
+			if (temp == 13)        // 엔터키 입력받았을 때
+			{
+				area = 2;
+				SelectPlane();     // 비행기 선택 화면으로 이동
+			}
+		}
 	}
 }
 
-int plane1[3][3] = { {1,0,1}, {0,1,0}, {1,0,1} }; //X
-int plane2[3][3] = { {1,0,1}, {1,1,1}, {1,0,1} }; //H
-int plane3[3][3] = { {1,1,1}, {0,1,0}, {0,1,0} }; //T
-int plane4[3][3] = { {0,1,0}, {1,0,1}, {0,1,0} }; //O
-//비행기 종 류
-
-void PrintPlane(int arr[][3], int x, int y) {
+void PrintPlane(int arr[][3][3], int type, int x, int y)
+{
+	// 비행기 프린트 해주는 함수
 	Gotoxy(x, y);
 	for (int i = 0; i < 3; i++)
 	{
 		for (int j = 0; j < 3; j++)
 		{
-			if (arr[i][j] = 1)
+			if (arr[type][i][j] == 1)
 			{
 				Gotoxy(x + j, y + i);
-				SetColor(White, Black);
-				if (x == 20)
+				if (type == 0)
+				{
+					SetColor(Yellow, Black);
 					printf("X");
-				else if (x == 30)
+				}
+				else if (type == 1)
+				{
+					SetColor(Lightblue, Black);
 					printf("H");
-				else if (x == 40)
+				}
+				else if (type == 2)
+				{
+					SetColor(Lightcyan, Black);
 					printf("T");
-				else 
+				}
+				else //type==3
+				{
+					SetColor(White, Black);
 					printf("O");
+				}
 			}
 		}
-
 	}
 }
-// 비행기 프린트 해주는 함수
 
-void SelectPlaneUI()
+void GetPlaneArrowKey()
 {
-	//TODO
+	int idx = 0;                  // 범위: 0~3
+	ArrowCursor(planeCursor[0], 2);  // 초기 커서 위치
+	while (area == 2)
+	{
+		int temp;
+		temp = _getch();
+		if (temp == 224)          // 방향키를 입력받았을 때
+		{
+			temp = _getch();
+			if (temp == 75)       // 왼쪽 방향키
+			{
+				if (idx != 0)
+				{
+					DeleteCursor(planeCursor[idx--], 2);
+					ArrowCursor(planeCursor[idx], 2);
+					continue;
+				}
+			}
+			else if (temp == 77)   // 오른쪽 방향키
+			{
+				if (idx != 3)
+				{
+					DeleteCursor(planeCursor[idx++], 2);
+					ArrowCursor(planeCursor[idx], 2);
+					continue;
+				}
+			}
+		}
+		else if (temp == 13)    // 엔터키를 입력받았을 때
+		{
+			area = 3;
+			planeCode = idx;
+			break;
+		}
+	}
+	ShootingGameUI();
 }
 
 void SelectPlane()
 {
-	//TODO
+	system("cls");
+	Gotoxy(20, 7); printf("방향키를 이용해 플레이할 비행기를 선택하세요.");   // 출력 안 됨 TODO
+
+	PrintPlane(plane, 0, 18, 10);
+	PrintPlane(plane, 1, 38, 10);
+	PrintPlane(plane, 2, 58, 10);
+	PrintPlane(plane, 3, 78, 10);
+
+	GetPlaneArrowKey();
+}
+
+void ShootingGameUI()
+{
+	return;
+}
+
+void ShootingGame()
+{
+
 }
 
 void RankingUI()
@@ -265,8 +422,8 @@ int main(void)
 	SetConsoleSize();  // 화면 크기 설정
 	DrawTitle();       // 타이틀 출력
 	PrintMenu();       // 메인 메뉴 출력
-	ArrowCursor(25);   // 메인 메뉴 커서 출력
-	GetArrowKey();     // 커서 이동 함수
+	ArrowCursor(25, 1);   // 메인 메뉴 커서 출력
+	GetTitleArrowKey();     // 커서 이동 함수
 
 	return 0;
 }
